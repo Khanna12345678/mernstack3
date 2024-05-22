ed135165660a7e8daf5e7a2b8c1fff33458088d5
Khanna12345678 <131513389+Khanna12345678@users.noreply.github.com>
2024-05-22 19:23:57 +0530

    update server

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 import app from "./app.js";
 import { connectDB } from "./config/database.js";
 import Razorpay from "razorpay";
+import path from "path";
+import { fileURLToPath } from "url";
 connectDB();
 const port = process.env.PORT || 5000;
    
+
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
 export const instance = new Razorpay({
   key_id: process.env.RAZORPAY_API_KEY,
   key_secret: process.env.RAZORPAY_API_SECRET,
 });
 
-app.get("/",(req,res,next)=>{
-    res.send("Working");
-})
-
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+});
 app.listen(port, () => {
-  console.log(`Server is working on PORT: ${port} in ${process.env.NODE_ENV} MODE`);
+  console.log(`Server is working on PORT: ${port} in ${process.env.NODE_ENV} MODE`);
 });
